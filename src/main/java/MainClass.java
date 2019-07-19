07d6b7c8f77f0cedd88db98f4b15f115cc5a7bc9
VVV <vl4d.vovik@yandex.ru>
2019-07-19 14:52:34 +0300

    Initial Commit

---
+++ src/main/java/MainClass.java
@@ -0,0 +1,10 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class MainClass {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\vvoitehovici\\IdeaProjects\\testdisportal\\drivers\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.com/");
+    }
+}
