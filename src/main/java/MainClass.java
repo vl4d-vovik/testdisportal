572b89797d62514b212fe454d173246f083d932b
VVV <vl4d.vovik@yandex.ru>
2019-07-22 20:58:38 +0300

    111

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -1,10 +1,20 @@
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+
 public class MainClass {
-    public static void main(String[] args) {
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\vvoitehovici\\IdeaProjects\\testdisportal\\drivers\\chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://www.google.com/");
+//    public static void main(String[] args) {
+//        System.setProperty("webdriver.chrome.driver", "C:\\Users\\vvoitehovici\\IdeaProjects\\testdisportal\\drivers\\chromedriver.exe");
+//        WebDriver driver = new ChromeDriver();
+//        driver.get("https://www.google.com/");
+//    }
+
+    @Test
+    public void test1() {
+        System.out.println("testOK");
     }
+
+
 }
